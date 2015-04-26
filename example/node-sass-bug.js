d86266051b909f51d9af7bcea98352623c34fa70
James Newell <james@digitaledgeit.com.au>
2015-04-26 19:13:44 +1000

    added shebang line

--- example/node-sass-bug.js
+++ example/node-sass-bug.js
@@ -1,26 +1,26 @@
-var sass = require('node-sass');
+    var sass = require('node-sass');
 
-var i = 0;
+    var i = 0;
 
-sass.render(
-  {
-    data: '@import "foobar"; @import "foobar";',
-    importer: function(file, prev, cb) {
-      console.log(file, prev);
+    sass.render(
+      {
+        data: '@import "foobar"; @import "foobar";',
+        importer: function(file, prev, cb) {
+          console.log(file, prev);
 
-      if (i++ === 0) {
-        cb({contents: '.foobar {color: red;}'});
-      } else {
-        cb({contents: ''});
+          if (i++ === 0) {
+            cb({contents: '.foobar {color: red;}'});
+          } else {
+            cb({contents: ''});
+          }
+
+        }
+      },
+      function(err, result) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(result.css.toString());
+        }
       }
-
-    }
-  },
-  function(err, result) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(result.css.toString());
-    }
-  }
-);
+    );
