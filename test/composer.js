f2e878aa84c01bd5b4707b16485ffed91ae02912
James Newell <james@digitaledgeit.com.au>
2015-08-23 12:44:17 +1000

    fix error handling on the stream when a callback is provided

--- test/composer.js
+++ test/composer.js
@@ -1,21 +1,33 @@
-var assert    = require('assert');
-var rule      = require('./lib/get-rule');
-var fixture   = require('./lib/get-fixture');
-var composer  = require('..');
+var assert = require('assert');
+var fixture = require('./lib/get-fixture');
+var composer = require('..');
 
 describe('Composer', function() {
   describe('Composer', function() {
-    describe('Context', function () {
 
-      it('should contain the full path', function(done) {
+    describe('.compose()', function() {
+
+      it('should call the callback with an error', function(done) {
 
         composer()
-          .entry(fixture('import-once'))
-          .importer(function(context, next) {
-            next(null, context); //assert should be the full path
+          .entry(fixture('syntax-error'))
+          .compose(function(err, css, stats) {
+            assert.notEqual(err, null);
+            assert.equal(css, null);
+            assert.equal(stats, null);
+            done();
           })
-          .compose(function(err, css) {
-            if (err) return done(err);
+        ;
+
+      });
+
+      it('should emit an error on the stream', function(done) {
+
+        composer()
+          .entry(fixture('syntax-error'))
+          .compose()
+          .on('error', function(err) {
+            assert.notEqual(err, null);
             done();
           })
         ;
@@ -23,5 +35,6 @@ describe('Composer', function() {
       });
 
     });
+
   });
 });
