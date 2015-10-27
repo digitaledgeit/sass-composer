b147e90554151773e3c27648cb3eaa5a57b95041
James Newell <j.newell@nib.com.au>
2015-10-27 16:37:37 +1100

    remove broken/unused test to make the tests pass

--- test/importers/node.js
+++ test/importers/node.js
@@ -5,10 +5,6 @@ describe('sass-composer', function() {
   describe('importer', function() {
     describe('node', function () {
 
-      it('', function(done) {
-        importer();
-      });
-
     });
   });
 });
